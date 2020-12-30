bcfd976631f8b26c5e5c09bb3a571f2014817beb
KotletJB <bronowski.jan2@gmail.com>
2020-12-30 15:49:39 +0100

    porzadki

--- bot.py
+++ bot.py
@@ -2,9 +2,7 @@ from os import environ
 import discord
 from discord.ext import commands
 import asyncio
-from among_us_code import AmongUs
-from shaker import Shaker
-from join_msgs import JoinSound
+
 from massmove import MassMove
 # from snooper import Snooper
 
@@ -21,9 +19,6 @@ intents.members = True
 intents.reactions = True
 intents.guilds = True
 client = commands.Bot(command_prefix = '.', intents = intents)
-client.add_cog(AmongUs(client))
-client.add_cog(Shaker(client))
-client.add_cog(JoinSound(client))
 client.add_cog(MassMove(client))
 # Run bot
 client.run(TOKEN)
